7eb569f9052d181997a66ca7c97c2a1acf6a8d34
louxinye <1061566571@qq.com>
2018-10-16 16:53:29 +0800

    update

--- center/bot_msg.py
+++ center/bot_msg.py
@@ -503,8 +503,15 @@ def MsgCenter(bot, context):
                     bot.send_group_msg(group_id=group_id, message=msg)
 
         # 主群超限检测
-        if context['message_type'] == 'group' and context['group_id'] in bot_global.group_main_list and context['user_id'] not in bot_global.ignore_list:
-            if context['user_id'] in bot_global.user_check_in_list:
+        if context['message_type'] == 'group' and context['group_id'] in bot_global.group_main_list:
+            member = bot_msgcheck.getGroupMemberInfo(bot, context['group_id'], context['user_id'])
+            if not member:
+                pass
+            elif context['user_id'] in bot_global.ignore_list or context['user_id'] in bot_global.white_list:
+                pass
+            elif member['role'] == 'admin' or member['role'] == 'owner':
+                pass
+            elif context['user_id'] in bot_global.user_check_in_list:
                 pass
             else:
                 bot_global.sql_action_lock.acquire()
